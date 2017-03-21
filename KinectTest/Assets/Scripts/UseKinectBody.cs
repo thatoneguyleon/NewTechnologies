8f7bcd5e025d2d29add76f81158ebad8f185b139
corderoit <tariquake@hotmail.com>
2017-03-21 18:44:25 +0100

    documentatie

--- KinectTest/Assets/Scripts/UseKinectBody.cs
+++ KinectTest/Assets/Scripts/UseKinectBody.cs
@@ -18,14 +18,14 @@ public class UseKinectBody : MonoBehaviour
     {
         GameObject[] AllGameObjects = Resources.FindObjectsOfTypeAll(typeof(GameObject)) as GameObject[];
         for(int i = 0; i < AllGameObjects.Length; i++) {
-            if (AllGameObjects[i].name.Contains("Body:")) {
+            if (AllGameObjects[i].name.Contains("Body:")) { // wanneer de kinect een lichaam herkend maakt hij een object aan met een naam waar het stuk "Body:" inzit, wanneer hij dat vind gaan we de paddle maken.
                 MakePlatform();
             }
         }
     }
 
     void MakePlatform() {        
-        if(GameObject.Find("HandLeft") && GameObject.Find("HandRight")) {
+        if(GameObject.Find("HandLeft") && GameObject.Find("HandRight")) { // als we beide handen kunnen zien gaan we die posities gebruiken.
             Vector3 HandLeft;
             Vector3 HandRight;
             HandLeft = GameObject.Find("HandLeft").transform.position;
@@ -34,10 +34,10 @@ public class UseKinectBody : MonoBehaviour
             Vector3 tLeftHand = new Vector3(HandLeft.x, HandRight.y - 15, 0);
             Vector3 tRightHand = new Vector3(HandRight.x, HandRight.y - 15, 0);
 
-            Vector3 center = Vector3.Lerp(tLeftHand, tRightHand, 0.5f);
-            float distance = Vector3.Distance(tLeftHand, tRightHand);
-            float mappedPosX = ExtensionMethods.Map(center.x, -11, 11, -50, 50);
-            float mappedPosY = ExtensionMethods.Map(center.y, -6, -16, -3, -16);
+            Vector3 center = Vector3.Lerp(tLeftHand, tRightHand, 0.5f); // we gebruiken een cube en hun pivot zit in de center.
+            float distance = Vector3.Distance(tLeftHand, tRightHand);   // de distance tussen twee handen gebruiken we voor de length.
+            float mappedPosX = ExtensionMethods.Map(center.x, -11, 11, -50, 50); // de world space is groter dan de real space, dus we moeten de positie mappen om de volledige breedte van de scene te benuttigen.
+            float mappedPosY = ExtensionMethods.Map(center.y, -6, -16, -3, -16); // we willen wat force gebruiken dus deze mappen we ook een klein beetje zodat je niet je armen er af hoeft te gooien.
 
             cube.transform.position = new Vector3(mappedPosX, center.y, center.z + cube.GetComponent<Paddle>().currentZ);
             cube.transform.localScale = new Vector3(distance, cube.transform.localScale.y, 1);
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # NewTechnologies
+
+Hoe heb je het aangepakt (gebruikershandleiding)
+Welke technologien heb je gebruikt
+Wat waren problemen 
+Wat werkt niet
+Wat is moeilijk
+
+<h2>Methode Beschrijving, Kinect Test</h2>
+Allereerst hebben we een tool uitgekozen, dat werd de kinect. We gingen met elkaar brainstormen over concepten die we zouden kunnen testen. Bij elk concept checkte we het internet of het al niet bestond. Het eerste concept was een level op iemands gezicht, d.m.v. facetracking. De kinect had als het goed is de unity libraries, maar er was geen test scene, dus wij wisten niet hoe het te moeten gebruiken.
+
+Waar wel een test scene van te vinden was, was de lichaam detectie. Wanneer de kinect een lichaam herkend maakt hij daar een 'skelet' van. Voor ons eerste hebben we arkanoid gemaakt, waarbij je handen de paddle bestuurt.
+
+<h>Problemen</h>
+In eerste instantie bleef de paddle horizontaal binnen de xy-plane. Om meer controle over je paddle te hebben wilde wij rotatie toevoegen, maar dat was vrij moelijk. 
+Wij hebben het volgende geprobeerd:
+- Vector3.Angle(linkerhand, rechterhand), het resultaat passen we toe op de paddle. Maar de paddle maakt willekeurige draaibewegingen. 
+- Vector3.LookAt(), hierdoor bleef de paddle niet binnen zijn xy-plane, wanneer je je handen een z-richting bewoog.
+
+We hebben het er maar uitgehaald. 
+
+Wat soms gebeurde wanneer je de paddle te hard omhoog bewoog is dat de bal er door heen bewoog. Dus de force die wij wilde toepassen, op basis van je beweegsnelheid, kon niet worden toegepast.
+
+Kinect kan tot 6 mensen herken, maar we wisten niet hoe we alle zes konden pakken om er eventueel een multiplayer van te maken.
